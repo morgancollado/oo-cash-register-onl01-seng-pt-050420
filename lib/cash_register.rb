06bde43c0a68eb2acc7de6b75dfb8228859c41f3
Morgan Collado <morgan.collado@gmail.com>
2020-05-27 23:57:36 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,9 @@
 class CashRegister
+  attr_accessor :total, :discount
   
-  def initialize(discount = 20)
+  def initialize(discount = nil)
     @total = 0 
+    @discount = discount
   end 
   
 end 
