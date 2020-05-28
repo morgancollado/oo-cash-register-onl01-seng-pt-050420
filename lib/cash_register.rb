cc63ca08e784f12f966395f9dcb827ad4a70245b
Morgan Collado <morgan.collado@gmail.com>
2020-05-28 00:19:54 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -23,7 +23,7 @@ class CashRegister
   end 
   
   def apply_discount
-    if !discount = nil
+    if discount != nil
     @total -= total * (discount.to_f/100)
     "After the discount, the total comes to $#{@total.to_i}."
     else "There is no discount to apply." 
